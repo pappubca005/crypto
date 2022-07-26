186d113d356d26734eabe3d07e6d4868c5c86567
Pappu Yadav <pappu.bca005.nshm@gmail.com>
2022-07-26 20:48:29 +0530

    REMOVED SETTING.PY frpm root

--- crypto/settings.py
+++ crypto/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 import os
 
@@ -127,5 +128,4 @@ STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
 django_heroku.settings(locals())
